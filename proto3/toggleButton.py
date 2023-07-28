59341f4a0e6f49dce5338c1e62a0ed48d2033635
SharmadG <mayureshgondkar@gmail.com>
2023-07-28 14:10:22 +0530

    update

--- proto3/toggleButton.py
+++ proto3/toggleButton.py
@@ -18,23 +18,43 @@ class togButton(QMainWindow):
         self.background_image.setPixmap(QPixmap("images/background.png"))
         self.background_image.setGeometry(0, 0, self.width, self.height)
 
-        self.toggle_but = QPushButton(self)
-        self.toggle_but.setGeometry(17, 246, 100, 100)
-        self.toggle_but.setCheckable(True)
-        self.toggle_but.clicked.connect(self.toggle)
+        self.Crd_but = QPushButton(self)
+        self.Crd_but.setGeometry(17, 246, 100, 100)
+        self.Crd_but.setCheckable(True)
+        self.Crd_but.clicked.connect(self.button_tog)
         icon = QIcon("images/icons/Card.png")
-        self.toggle_but.setIcon(icon)
+        self.Crd_but.setIcon(icon)
         icon_size = QSize(90, 90)
-        self.toggle_but.setIconSize(icon_size)
+        self.Crd_but.setIconSize(icon_size)
 
-    def toggle(self):
-        if self.toggle_but.isChecked():
+        self.finger_but = QPushButton(self)
+        self.finger_but.setGeometry(17, 372, 100, 100)
+        self.finger_but.setCheckable(True)
+        self.finger_but.clicked.connect(self.fing_tog)
+        icon = QIcon("images/icons/FingerVeri_Icon.png")
+        self.finger_but.setIcon(icon)
+        icon_size = QSize(90, 90)
+        self.finger_but.setIconSize(icon_size)
 
-            self.toggle_but.setStyleSheet("QPushButton {background-color:lightgreen}")
+
+    def button_tog(self):
+        if self.Crd_but.isChecked():
+
+            self.Crd_but.setStyleSheet("QPushButton {background-color:lightgreen}")
 
         else:
 
-            self.toggle_but.setStyleSheet("QPushButton {background-color:lightcoral}")
+            self.Crd_but.setStyleSheet("QPushButton {background-color:lightcoral}")
+
+    def fing_tog(self):
+            if self.finger_but.isChecked():
+
+                self.finger_but.setStyleSheet("QPushButton {background-color:lightgreen}")
+
+            else:
+
+                self.finger_but.setStyleSheet("QPushButton {background-color:lightcoral}")
+
 
 
         #self.canteen = imgbutton2(self, "images/icons/Card.png", 100, 100, [17, 246], self.close)
